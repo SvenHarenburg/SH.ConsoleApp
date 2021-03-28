ccbee1bb6d53dc772fe03c2f4c530c654c174dd0
Sven Harenburg <sven@svenharenburg.de>
2021-03-28 12:30:47 +0200

    Added indent-parameter to utility-function WriteDictionary. Helps to provide better output-formatting.

--- SH.ConsoleApp/ConsoleUtilities.cs
+++ SH.ConsoleApp/ConsoleUtilities.cs
@@ -13,14 +13,19 @@ namespace SH.ConsoleApp
     /// </summary>
     /// <param name="dictionary">The <see cref="Dictionary{string, string}"/> containing keys and values to write.</param>
     /// <param name="spaces">The amount of spaces to be written between keys and values.</param>
+    /// <param name="indent">The amount of spaces to be written before each line. Can be used to indent the output for better formatting.</param>
     /// <remarks>Very basic for now. Does not yet implement extended functionality like linebreaks.</remarks>
-    public static void WriteDictionary(Dictionary<string, string> dictionary, int spaces = 2)
+    public static void WriteDictionary(Dictionary<string, string> dictionary, int spaces = 2, int indent = 0)
     {
       if (dictionary == null) throw new ArgumentNullException(nameof(dictionary));
 
       var longestKey = dictionary.Keys.Max(q => q.Length);      
       foreach(var item in dictionary)
       {
+        for (int i = 0; i < indent; i++)
+        {
+          Console.Write(" ");
+        }
         Console.Write(item.Key.PadRight(longestKey + spaces));
         Console.Write(item.Value);
         Console.WriteLine();
