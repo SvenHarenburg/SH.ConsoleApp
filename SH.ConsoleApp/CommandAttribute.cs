ef4fdaab4dafa8ef3b340a4588aecfc3fdd9cbf8
Sven Harenburg <sven@svenharenburg.de>
2021-03-28 12:36:40 +0200

    CommandAttribute now inherits from CommandAttributeBaseWithDescription so it does not have to implement the Description property itself.

--- SH.ConsoleApp/CommandAttribute.cs
+++ SH.ConsoleApp/CommandAttribute.cs
@@ -7,15 +7,13 @@ using System.Threading.Tasks;
 namespace SH.ConsoleApp
 {
   [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
-  public class CommandAttribute : CommandAttributeBase
+  public class CommandAttribute : CommandAttributeBaseWithDescription
   {
-    public string Description { get; set; }
-
     public CommandAttribute() : base()
     {
     }
 
-    public CommandAttribute(string name, string description) : base(name)
+    public CommandAttribute(string name, string description) : base(name, description)
     {
       Description = description;
     }
