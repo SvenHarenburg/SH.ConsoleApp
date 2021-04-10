e412ef39a1b9d8128c3b917fb4d74a663384bfc6
Sven Harenburg <sven@svenharenburg.de>
2021-04-10 12:35:02 +0200

    Added tests for CommandTree

---
+++ SH.ConsoleApp.Tests/Core/CommandTreeTests.cs
@@ -0,0 +1,91 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SH.ConsoleApp.Core;
+using System.Reflection;
+using SH.ConsoleApp.Tests.TestCommands;
+
+namespace SH.ConsoleApp.Tests.Core
+{
+  [TestFixture]
+  public class CommandTreeTests
+  {
+
+    [TestFixture]
+    public class FindCommandTests
+    {
+      private CommandTree _tree;
+
+      [SetUp]
+      public void Setup()
+      {
+        // The creation of a CommandTree is complicated. To keep the test simple, the CommandTreeBuilder is used.        
+        // The tradeoff is that if the CommandTreeBuilder does not work correctly, this test might fail even though
+        // the CommandTree itself works correctly. Since the Builder itself is tested, I find this acceptable.
+        _tree = new CommandTreeBuilder(Assembly.GetExecutingAssembly()).BuildBaseTree();
+      }
+
+      [Test]
+      public void FindsMatchNoOptionsOrArguments()
+      {
+        var expectedMethod = typeof(CommandTreeTestsCommand).GetMethod(nameof(CommandTreeTestsCommand.NoOptionsOrArguments));
+        var match = _tree.FindCommand("CommandTreeTestsCommand NoOptionsOrArguments",
+          options: new List<string>(),
+          arguments: new List<string>());
+        Assert.AreEqual(match.Command.CommandMethodInfo.MethodHandle, expectedMethod.MethodHandle);
+      }
+
+      [Test]
+      public void FindsMatchWithOptionsNoArguments()
+      {
+        var expectedMethod = typeof(CommandTreeTestsCommand).GetMethod(nameof(CommandTreeTestsCommand.WithOptionsNoArguments));
+        var match = _tree.FindCommand("CommandTreeTestsCommand WithOptionsNoArguments",
+          options: new List<string>() { "option1", "option2" },
+          arguments: new List<string>());
+        Assert.AreEqual(match.Command.CommandMethodInfo.MethodHandle, expectedMethod.MethodHandle);
+      }
+
+      [Test]
+      public void FindsMatchNoOptionsWithArguments()
+      {
+        var expectedMethod = typeof(CommandTreeTestsCommand).GetMethod(nameof(CommandTreeTestsCommand.NoOptionsWithArguments));
+        var match = _tree.FindCommand("CommandTreeTestsCommand NoOptionsWithArguments",
+          options: new List<string>(),
+          arguments: new List<string>() { "argument1", "argument2" });
+        Assert.AreEqual(match.Command.CommandMethodInfo.MethodHandle, expectedMethod.MethodHandle);
+      }
+
+      [Test]
+      public void FindsMatchWithOptionsAndArguments()
+      {
+        var expectedMethod = typeof(CommandTreeTestsCommand).GetMethod(nameof(CommandTreeTestsCommand.WithOptionsAndArguments));
+        var match = _tree.FindCommand("CommandTreeTestsCommand WithOptionsAndArguments",
+          options: new List<string>() { "option1", "option2" },
+          arguments: new List<string>() { "argument1", "argument2" });
+        Assert.AreEqual(match.Command.CommandMethodInfo.MethodHandle, expectedMethod.MethodHandle);
+      }
+
+      [Test]
+      public void FindsBestMatchWithMultiplePotentialCommands()
+      {
+        var expectedMethod = typeof(CommandTreeTestsCommand).GetMethod(nameof(CommandTreeTestsCommand.MultiplePotentialCommands1));
+        var match = _tree.FindCommand("CommandTreeTestsCommand MultiplePotentialCommands",
+          options: new List<string>() { "option1" },
+          arguments: new List<string>() { "argument1" });
+        Assert.AreEqual(match.Command.CommandMethodInfo.MethodHandle, expectedMethod.MethodHandle);
+      }
+
+      [Test]
+      public void ReturnsNullOnNoMatch()
+      {
+        var match = _tree.FindCommand("CommandTreeTestsCommand NonExistingCommand",
+          options: new List<string>(),
+          arguments: new List<string>());
+        Assert.IsNull(match);
+      }
+    }
+  }
+}
---
+++ SH.ConsoleApp.Tests/TestCommands/CommandTreeTestsCommand.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SH.ConsoleApp;
+
+namespace SH.ConsoleApp.Tests.TestCommands
+{
+
+  [CommandGroup("CommandTreeTestsCommand", "")]
+  public class CommandTreeTestsCommand
+  {
+    [Command(nameof(NoOptionsOrArguments), "")]
+    public void NoOptionsOrArguments()
+    {
+    }
+
+    [Command(nameof(WithOptionsNoArguments), "")]
+    public void WithOptionsNoArguments(
+      [CommandOption("option1", "")] string option1,
+      [CommandOption("option2", "")] string option2
+      )
+    {
+    }
+
+    [Command(nameof(NoOptionsWithArguments), "")]
+    public void NoOptionsWithArguments(
+      [CommandArgument("argument1", "")] string argument1,
+      [CommandArgument("argument2", "")] string argument2
+      )
+    {
+    }
+
+    [Command(nameof(WithOptionsAndArguments), "")]
+    public void WithOptionsAndArguments(
+      [CommandOption("option1", "")] string option1,
+      [CommandOption("option2", "")] string option2,
+      [CommandArgument("argument1", "")] string argument1,
+      [CommandArgument("argument2", "")] string argument2
+      )
+    {
+    }
+
+    [Command("MultiplePotentialCommands", "")]
+    public void MultiplePotentialCommands1(
+      [CommandOption("option1", "")] string option1,
+      [CommandArgument("argument1", "")] string argument1,
+      [CommandOption("option2", "")] string option2 = ""
+      )
+    {
+    }
+
+    [Command("MultiplePotentialCommands", "")]
+    public void MultiplePotentialCommands2(
+      [CommandOption("option1", "")] string option1,
+      [CommandArgument("argument1", "")] string argument1,
+      [CommandOption("option2", "")] string option2 = "",
+      [CommandOption("option3", "")] string option3 = "",
+      [CommandArgument("argument2", "")] string argument2 = ""
+      )
+    {
+    }
+
+  }
+}
