586baea4a71ee45516e47a1f373a5e0b60d0f041
Sven Harenburg <sven@svenharenburg.de>
2021-04-24 07:36:36 +0200

    New StructExtension: IsDefault<T>
    Provides a more readable way to check if the value is the default-value of a struct.
    Thanks to https://stackoverflow.com/a/5635729

---
+++ SH.ConsoleApp/Extensions/StructExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SH.ConsoleApp.Extensions
+{
+  public static class StructExtensions
+  {
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="value"></param>
+    /// <returns></returns>
+    /// <remarks>Thanks to https://stackoverflow.com/a/5635729 </remarks>
+    public static bool IsDefault<T>(this T value) where T : struct
+    {
+      return value.Equals(default(T));
+    }
+  }
+}
